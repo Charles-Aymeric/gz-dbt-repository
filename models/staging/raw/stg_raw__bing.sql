64865a0a0e562f02f6d420150feafe8bc9fbaa0c
Charles-Aymeric Palard <ca.palard@gmail.com>
2024-02-07 14:46:23 +0000

    ex3-11

---
+++ models/staging/raw/stg_raw__bing.sql
@@ -0,0 +1,20 @@
+with
+
+    source as (select * from {{ source("raw", "bing") }}),
+
+    raw_bing as (
+
+        select 
+        date_date,
+        paid_source,
+        campaign_key,
+        camPNG_name as campaign_name,
+        CAST(ads_cost as FLOAT64) as ads_cost,
+        impression,
+        click
+        from source
+
+    )
+
+select *
+from raw_bing
