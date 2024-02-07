cbb5fe8829a9d002f6cbe52609a6efab0b2c171b
Charles-Aymeric Palard <ca.palard@gmail.com>
2024-02-07 14:50:15 +0000

    3.1.2

--- models/staging/raw/stg_raw__facebook.sql
+++ models/staging/raw/stg_raw__facebook.sql
@@ -8,7 +8,7 @@ with
         date_date,
         paid_source,
         campaign_key,
-        camPNG_name as campaign_name,
+        camPGN_name as campaign_name,
         CAST(ads_cost as FLOAT64) as ads_cost,
         impression,
         click
