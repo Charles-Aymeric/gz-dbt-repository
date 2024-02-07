c38706a10b0d5a9170e204e6ff4cbcae1a28eabc
Charles-Aymeric Palard <ca.palard@gmail.com>
2024-02-07 10:35:43 +0000

    solution ex4

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,11 +1,12 @@
 SELECT 
-    date_date,
+    products_id, 
+    date_date, 
     orders_id,
-    sales.products_id,
-    revenue,
-    quantity,
-    products.purchase_price,
-    (products.purchase_price*sales.quantity) AS purchase_cost
-FROM {{ ref("stg_raw__sales")}} sales
-JOIN {{ ref("stg_raw__product")}} products
-    ON  products.products_id = sales.products_id
+    revenue, 
+    quantity, 
+    CAST(purchase_price AS FLOAT64) as purchase_price, 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    ROUND(s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2),2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p 
+    USING (products_id)
